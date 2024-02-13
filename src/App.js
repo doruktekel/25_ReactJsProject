1dbc6e3aa398e6fa95b3efcf1a9e9e9b74723e69
doruk.tekel <doruk.tekel@bilgeadam.com>
2024-02-13 21:47:02 +0300

    rating stars was done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Accordion from "./components/accordion";
 import GenerateColor from "./components/generate_color";
+import StarRating from "./components/rating";
 
 function App() {
   return (
     <div className="App">
       {/* <Accordion /> */}
-      <GenerateColor />
+      {/* <GenerateColor /> */}
+      <StarRating />
     </div>
   );
 }
--- src/components/generate_color/index.jsx
+++ src/components/generate_color/index.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "./style.css";
 
 const GenerateColor = () => {
   const [type, setType] = useState("hex");
